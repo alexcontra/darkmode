64aff43f089e05d3848171a79129c68f14f21cd9
Alex Contra <contraalex8@gmail.com>
2021-07-27 13:24:16 +0300

    create class for lightmode

---
+++ lib/screens/lightmode_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LightMode extends StatefulWidget {
+  const LightMode({Key? key}) : super(key: key);
+
+  @override
+  _LightModeState createState() => _LightModeState();
+}
+
+class _LightModeState extends State<LightMode> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
